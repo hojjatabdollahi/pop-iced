27deab60aa22fbfde9beddbbcaa5aefc9f69e8b3
Héctor Ramón Jiménez <hector@hecrj.dev>
2026-01-28 06:34:03 +0100

    Simplify clip path creation in `tiny_skia`

--- tiny_skia/src/engine.rs
+++ tiny_skia/src/engine.rs
@@ -837,20 +837,15 @@ fn rounded_box_sdf(
 pub fn adjust_clip_mask(clip_mask: &mut tiny_skia::Mask, bounds: Rectangle) {
     clip_mask.clear();
 
-    let path = {
-        let mut builder = tiny_skia::PathBuilder::new();
-        builder.push_rect(
-            tiny_skia::Rect::from_xywh(
-                bounds.x,
-                bounds.y,
-                bounds.width,
-                bounds.height,
-            )
-            .unwrap(),
-        );
-
-        builder.finish().unwrap()
-    };
+    let path = tiny_skia::PathBuilder::from_rect(
+        tiny_skia::Rect::from_xywh(
+            bounds.x,
+            bounds.y,
+            bounds.width,
+            bounds.height,
+        )
+        .expect("Create clip rectangle"),
+    );
 
     clip_mask.fill_path(
         &path,
