julep-ui/julep-iced

Use `Overlay` to draw picked pane in `pane_grid`

widget/src/pane_grid.rs
@@ -764,8 +764,6 @@ where
             cursor
         };
 
-        let mut render_picked_pane = None;
-
         let pane_in_edge = if picked_pane.is_some() {
             cursor
                 .position()
@@ -790,10 +788,8 @@ where
             })
         {
             match picked_pane {
-                Some((dragging, origin)) if id == dragging => {
-                    render_picked_pane = Some(((content, tree), origin, pane_layout));
-                }
-                Some((dragging, _)) if id != dragging => {
+                Some((dragging, _)) if id == dragging => {}
+                Some(_) => {
                     content.draw(
                         tree,
                         renderer,
@@ -849,29 +845,6 @@ where
             );
         }
 
-        // Render picked pane last
-        if let Some(((content, tree), origin, layout)) = render_picked_pane
-            && let Some(cursor_position) = cursor.position()
-        {
-            let bounds = layout.bounds();
-
-            let translation = cursor_position - Point::new(origin.x, origin.y);
-
-            renderer.with_translation(translation, |renderer| {
-                renderer.with_layer(bounds, |renderer| {
-                    content.draw(
-                        tree,
-                        renderer,
-                        theme,
-                        defaults,
-                        layout,
-                        pane_cursor,
-                        viewport,
-                    );
-                });
-            });
-        }
-
         if picked_pane.is_none()
             && let Some((axis, split_region, is_picked)) = picked_split
         {
@@ -914,6 +887,9 @@ where
         viewport: &Rectangle,
         translation: Vector,
     ) -> Option<overlay::Element<'b, Message, Theme, Renderer>> {
+        let state = tree.state.downcast_ref::<Memory>();
+        let picked_pane = state.action.picked_pane();
+
         let children = self
             .panes
             .iter()
@@ -921,7 +897,7 @@ where
             .zip(&mut self.contents)
             .zip(&mut tree.children)
             .zip(layout.children())
-            .filter_map(|(((pane, content), state), layout)| {
+            .filter_map(|(((pane, content), tree), layout)| {
                 if self
                     .internal
                     .maximized()
@@ -930,11 +906,73 @@ where
                     return None;
                 }
 
-                content.overlay(state, layout, renderer, viewport, translation)
+                if let Some((picked_pane, origin)) = picked_pane
+                    && picked_pane == pane
+                {
+                    return Some(overlay::Element::new(Box::new(PickedPane {
+                        origin,
+                        content,
+                        tree,
+                        layout,
+                    })));
+                }
+
+                content.overlay(tree, layout, renderer, viewport, translation)
             })
             .collect::<Vec<_>>();
 
         (!children.is_empty()).then(|| Group::with_children(children).overlay())
+    }
+}
+
+struct PickedPane<'a, 'b, Message, Theme, Renderer>
+where
+    Theme: container::Catalog,
+    Renderer: core::Renderer,
+{
+    content: &'a Content<'b, Message, Theme, Renderer>,
+    origin: Point,
+    tree: &'a mut Tree,
+    layout: Layout<'a>,
+}
+
+impl<'a, 'b, Message, Theme, Renderer> core::Overlay<Message, Theme, Renderer>
+    for PickedPane<'a, 'b, Message, Theme, Renderer>
+where
+    Theme: container::Catalog,
+    Renderer: core::Renderer,
+{
+    fn layout(&mut self, _renderer: &Renderer, _bounds: Size) -> layout::Node {
+        // TODO: Mouse translation
+        layout::Node::new(self.layout.bounds().size()).move_to(self.origin)
+    }
+
+    fn draw(
+        &self,
+        renderer: &mut Renderer,
+        theme: &Theme,
+        style: &renderer::Style,
+        _layout: Layout<'_>,
+        cursor: mouse::Cursor,
+    ) {
+        let bounds = self.layout.bounds();
+
+        let translation =
+            cursor.position().unwrap_or_default() - Point::new(self.origin.x, self.origin.y);
+
+        renderer.with_translation(translation, |renderer| {
+            renderer.with_layer(bounds, |renderer| {
+                self.content.draw(
+                    self.tree,
+                    renderer,
+                    theme,
+                    style,
+                    self.layout,
+                    mouse::Cursor::Unavailable,
+                    &Rectangle::INFINITE,
+                );
+            });
+        });
     }
 }
 

hojjatabdollahi/pop-iced

widget/src/pane_grid.rs
@@ -848,8 +848,6 @@ where
             cursor
         };
 
-        let mut render_picked_pane = None;
-
         let pane_in_edge = if picked_pane.is_some() {
             cursor
                 .position()
@@ -874,11 +872,8 @@ where
             })
         {
             match picked_pane {
-                Some((dragging, origin)) if id == dragging => {
-                    render_picked_pane =
-                        Some(((content, tree), origin, pane_layout));
-                }
-                Some((dragging, _)) if id != dragging => {
+                Some((dragging, _)) if id == dragging => {}
+                Some(_) => {
                     content.draw(
                         tree,
                         renderer,
@@ -937,29 +932,6 @@ where
             );
         }
 
-        // Render picked pane last
-        if let Some(((content, tree), origin, layout)) = render_picked_pane
-            && let Some(cursor_position) = cursor.position()
-        {
-            let bounds = layout.bounds();
-
-            let translation = cursor_position - Point::new(origin.x, origin.y);
-
-            renderer.with_translation(translation, |renderer| {
-                renderer.with_layer(bounds, |renderer| {
-                    content.draw(
-                        tree,
-                        renderer,
-                        theme,
-                        defaults,
-                        layout,
-                        pane_cursor,
-                        viewport,
-                    );
-                });
-            });
-        }
-
         if picked_pane.is_none()
             && let Some((axis, split_region, is_picked)) = picked_split
         {
@@ -1005,6 +977,9 @@ where
         viewport: &Rectangle,
         translation: Vector,
     ) -> Option<overlay::Element<'b, Message, Theme, Renderer>> {
+        let state = tree.state.downcast_ref::<Memory>();
+        let picked_pane = state.action.picked_pane();
+
         let children = self
             .panes
             .iter()
@@ -1012,7 +987,7 @@ where
             .zip(&mut self.contents)
             .zip(&mut tree.children)
             .zip(layout.children())
-            .filter_map(|(((pane, content), state), c_layout)| {
+            .filter_map(|(((pane, content), tree), c_layout)| {
                 if self
                     .internal
                     .maximized()
@@ -1021,8 +996,18 @@ where
                     return None;
                 }
 
-                content.overlay(
-                    state,
+                if let Some((picked_pane, origin)) = picked_pane
+                    && picked_pane == pane
+                {
+                    return Some(overlay::Element::new(Box::new(PickedPane {
+                        origin,
+                        content,
+                        tree,
+                        layout,
+                    })));
+                }
+
+                content.overlay(tree,
                     c_layout.with_virtual_offset(layout.virtual_offset()),
                     renderer,
                     viewport,
@@ -1061,6 +1046,57 @@ where
                     dnd_rectangles,
                 );
             });
+    }
+}
+
+struct PickedPane<'a, 'b, Message, Theme, Renderer>
+where
+    Theme: container::Catalog,
+    Renderer: core::Renderer,
+{
+    content: &'a Content<'b, Message, Theme, Renderer>,
+    origin: Point,
+    tree: &'a mut Tree,
+    layout: Layout<'a>,
+}
+
+impl<'a, 'b, Message, Theme, Renderer> core::Overlay<Message, Theme, Renderer>
+    for PickedPane<'a, 'b, Message, Theme, Renderer>
+where
+    Theme: container::Catalog,
+    Renderer: core::Renderer,
+{
+    fn layout(&mut self, _renderer: &Renderer, _bounds: Size) -> layout::Node {
+        // TODO: Mouse translation
+        layout::Node::new(self.layout.bounds().size()).move_to(self.origin)
+    }
+
+    fn draw(
+        &self,
+        renderer: &mut Renderer,
+        theme: &Theme,
+        style: &renderer::Style,
+        _layout: Layout<'_>,
+        cursor: mouse::Cursor,
+    ) {
+        let bounds = self.layout.bounds();
+
+        let translation =
+            cursor.position().unwrap_or_default() - Point::new(self.origin.x, self.origin.y);
+
+        renderer.with_translation(translation, |renderer| {
+            renderer.with_layer(bounds, |renderer| {
+                self.content.draw(
+                    self.tree,
+                    renderer,
+                    theme,
+                    style,
+                    self.layout,
+                    mouse::Cursor::Unavailable,
+                    &Rectangle::INFINITE,
+                );
+            });
+        });
     }
 }
 